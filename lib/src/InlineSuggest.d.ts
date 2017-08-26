fe15f0a3563dcbaf530b49e27558f3750b1d5e18
xmazu <xmazu@yahoo.com>
2017-08-26 10:03:03 +0200

    publish: 0.5.0
    generated from commit f21fb9c0fff323f3f22aab030ced6313cd8dcc2b

--- lib/src/InlineSuggest.d.ts
+++ lib/src/InlineSuggest.d.ts
@@ -1,20 +1,32 @@
 /// <reference types="react" />
 import * as React from 'react';
 import { ReactElement } from 'react';
-import { SuggestType } from './Types';
 export declare namespace InlineSuggest {
-    type Props = React.HTMLProps<HTMLInputElement> & SuggestType.Props;
-    type State = SuggestType.State;
+    type Props = React.HTMLProps<HTMLInputElement> & {
+        value: string;
+        haystack: any[];
+        onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
+        getFn?: (obj: any) => string;
+        onMatch?: (v: string | any) => void;
+        ignoreCase?: boolean;
+        shouldRenderSuggestion?: (value: string | any) => boolean;
+        switchBetweenSuggestions?: boolean;
+    };
+    type State = {
+        matchedArray: any[];
+        needle: string;
+        activeIndex: number;
+    };
 }
 export declare class InlineSuggest extends React.Component<InlineSuggest.Props, InlineSuggest.State> {
-    static defaultProps: {
-        ignoreCase: boolean;
-    };
+    static defaultProps: InlineSuggest.Props;
     constructor(props: InlineSuggest.Props);
     render(): ReactElement<any>;
+    private renderSuggestion();
     private fireOnChange;
     private handleOnChange;
     private handleOnBlur;
     private handleOnKeyDown;
     private handleOnKeyUp;
+    private setNewActiveIndex;
 }
